cb9ba50ca3693cafc1397b6e35f309d6bcbfdcc5
Kirill <kir_zinn@mail.ru>
2021-10-30 10:21:45 +0300

    Added .gitignore + function IsLeapYear

---
+++ time.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <ctime>
+class DateTime
+{
+public:
+	int year;
+	int month;
+	int day;
+	int hour;
+	int min;
+	int sec;
+	int millisec;
+};
+bool IsLeapYear(DateTime t) 
+{
+	if (t.year % 100 != 0)
+	{
+		return (t.year % 4 == 0);
+	}
+	else
+		return (t.year % 400 == 0);
+}
