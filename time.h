15952f1a6a73d24b03b3b367c0cf67a3bb71791f
Kirill <kir_zinn@mail.ru>
2021-10-30 10:40:42 +0300

    added 2 functions DaysInYear and DaysInYearRange

--- time.h
+++ time.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <ctime>
+#include <cassert>
 class DateTime
 {
 public:
@@ -37,4 +38,36 @@ DateTime LaterInYear(DateTime t1, DateTime t2)
 	}
 	else
 		return t2;
+}
+int DaysInYear(DateTime t) 
+{
+	if (IsLeapYear(t))
+	{
+		return 366;
+	}
+	else return 365;
+}
+int DaysInYearRange(DateTime t1, DateTime t2) 
+{
+	assert(t1.year > t2.year);
+	int count = 0;
+	for (int i = t1.year; i < t2.year+1; i++)
+	{
+		if (i % 100 != 0)
+		{
+			if (i % 4 == 0)
+			{
+				count += 366;
+			}
+			else count += 365;
+		}
+		else 
+		{
+			if (i % 400 == 0)
+				count += 366;
+			else count += 365;
+
+		}
+	}
+	return count;
 }
